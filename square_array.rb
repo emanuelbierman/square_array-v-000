3dda73713c8078adae08f5c813f7fab7e60f1ff8
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-18 16:07:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  new_array = Array.new(array)
-  new_array.each do |element|
+  array.each do |element|
     element = element * element
   end
+  new_array = Array.new(array)
 end
